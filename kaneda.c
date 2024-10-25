07a9d0016ef5ecc599d0840374c8d61f2e94eb7b
KanedaTakeru <t236146@st.yamagata-u.ac.jp>
2024-10-25 14:17:57 +0900

    Create kaneda.c

---
+++ kaneda.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+
+int main()
+{
+ int k=3;
+    int t=4;
+    int sum=k+t;
+    printf("How many girl and boy?");
+    scanf("%d", &k);
+     
+    printf("The number of students is %d",sum);
+    
+
+    return 0;
+
+    return 0;
+	
+}
