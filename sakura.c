b2b8b2208e116cb615cd573aef8039bbb846fd52
KanedaTakeru <t236146@st.yamagata-u.ac.jp>
2025-01-24 19:04:55 +0900

    Add files via upload

---
+++ sakura.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+	int main()
+{  
+    int k;
+    printf("How many students?");
+    scanf("%d", &k);
+     
+    printf("The number of students is %d",k);
+    
+
+    return 0;
+}
+}
+
